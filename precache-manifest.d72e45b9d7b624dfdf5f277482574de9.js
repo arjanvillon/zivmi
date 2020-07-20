091397e69f001859d861a2274ec246870ed663e1
Arjan Heaven Villon <heaven.arjan@gmail.com>
2020-07-20 16:19:31 +0800

    Updates

---
+++ precache-manifest.d72e45b9d7b624dfdf5f277482574de9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5b2cf2d424e07abbd29ab7053b936c",
+    "url": "/zivmi/index.html"
+  },
+  {
+    "revision": "6f525156ceb36fa15b1b",
+    "url": "/zivmi/static/css/main.077252ab.chunk.css"
+  },
+  {
+    "revision": "9992ad5efdb1b1e00a6d",
+    "url": "/zivmi/static/js/2.35c3e93c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zivmi/static/js/2.35c3e93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f525156ceb36fa15b1b",
+    "url": "/zivmi/static/js/main.85a0c19f.chunk.js"
+  },
+  {
+    "revision": "c2ee0ad9253a6274f428",
+    "url": "/zivmi/static/js/runtime-main.fd7caec9.js"
+  },
+  {
+    "revision": "ab66db3447b50ca2d4403ac8803a5a54",
+    "url": "/zivmi/static/media/apple_button.ab66db34.svg"
+  },
+  {
+    "revision": "98f7dcbe7af8749848ba688e9fc1af2c",
+    "url": "/zivmi/static/media/egypt.98f7dcbe.svg"
+  },
+  {
+    "revision": "3d03a06f227e37b37be2ff1eaf13cc24",
+    "url": "/zivmi/static/media/google_button.3d03a06f.svg"
+  },
+  {
+    "revision": "8dd13334676841fb5055ad64b572fe5d",
+    "url": "/zivmi/static/media/person1.8dd13334.png"
+  },
+  {
+    "revision": "d2be1423a9de05f05bc37541681db826",
+    "url": "/zivmi/static/media/person2.d2be1423.png"
+  },
+  {
+    "revision": "f0403223465dfdc26ba09a8ebbf1b44b",
+    "url": "/zivmi/static/media/person3.f0403223.png"
+  },
+  {
+    "revision": "3fa2bc181ff755a4a3fb9525a5651e8a",
+    "url": "/zivmi/static/media/person4.3fa2bc18.png"
+  },
+  {
+    "revision": "5f98edcad9fb22826fcd097799f023ed",
+    "url": "/zivmi/static/media/person5.5f98edca.png"
+  },
+  {
+    "revision": "197876d6d2342f8bd2f450f8d31c3511",
+    "url": "/zivmi/static/media/phone1x2.197876d6.png"
+  },
+  {
+    "revision": "be2bf95ab581a7ab7538bc0de135474c",
+    "url": "/zivmi/static/media/phone2x2.be2bf95a.png"
+  },
+  {
+    "revision": "eecaade6a80b33934e421f0941b2858a",
+    "url": "/zivmi/static/media/phone3x2.eecaade6.png"
+  }
+]);
